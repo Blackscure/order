10cc6d97f0dd668a445bbc4b4e251ea9f4bc7aac
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-22 22:57:52 +0300

    customer and oders model added

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from customers.models import Customer
+
 # Create your models here.
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    item = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
